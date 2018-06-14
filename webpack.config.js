80dca6aca474db3ab9ef159006bb5317dbe5d991
DimaDK <375445814266@ya.ru>
2018-06-14 18:12:30 +0300

    Enable source maps

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
     entry: './src/index.js',
+    devtool: 'inline-source-map',
     output: {
         filename: 'index.js',
         path: path.resolve(__dirname, 'dist')
