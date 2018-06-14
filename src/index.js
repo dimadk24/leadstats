eff2003a0a6224a46cc0a3018520b47129b870b6
DimaDK <375445814266@ya.ru>
2018-06-14 18:12:16 +0300

    Basic select initialization

---
+++ src/index.js
@@ -0,0 +1,16 @@
+function onLoad() {
+    initSelect();
+}
+
+function initSelect() {
+    const select = $('select#account');
+    select.select2({placeholder: "Выбрать", language: "ru"});
+    select.on('select2:select', onAdCabinetSelect);
+}
+
+function onAdCabinetSelect(e) {
+    const id = e.params.data.id;
+    console.log(id);
+}
+
+$(document).ready(onLoad);
