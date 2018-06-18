32f3d55d53446e8f7f701070059acd0362e65436
DimaDK <375445814266@ya.ru>
2018-06-18 17:26:47 +0300

    read file into js data, added basic error messages and other related improvements

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import CSV from 'comma-separated-values/csv';
+
 let ad_cabinet_id = 0;
 let file_content = '';
 
@@ -9,7 +11,11 @@ function onLoad() {
 function initSelect() {
     const select = $('select#ad-acc-select');
     select.select2({placeholder: "Выбрать", language: "ru"});
-    select.on('select2:select', (e) => ad_cabinet_id = e.params.data.id);
+    load_select_data();
+    select.on('select2:select', (e) => {
+        ad_cabinet_id = e.params.data.id;
+        work();
+    });
 }
 
 const dropzone_hover_class = 'dropzone-hover';
@@ -27,17 +33,47 @@ function dragOver(e) {
     e.preventDefault();
 }
 
+function work() {
+    if (ad_cabinet_id && file_content) {
+        let csv = new CSV(remove_header(file_content), {'header': true});
+        console.log(csv.parse());
+    }
+}
+
+function remove_header(text) {
+    return text.split('\n\n')[1]
+}
+
 function readFile(file) {
     const reader = new FileReader();
-    reader.onload = (e) => file_content = e.target.result;
+    reader.onload = (e) => {
+        file_content = e.target.result;
+        work();
+    };
     reader.readAsText(file)
+}
+
+function safe_get_file(dataTransfer) {
+    const files = dataTransfer.files;
+    let error = '';
+    if (!files.length) {
+        error = 'Перетащи файл';
+    }
+    if (!files[0].name.endsWith('.csv')) {
+        error = 'Неверный файл!\nУ него расширение не .csv';
+    }
+    if (error){
+        alert(error);
+        throw new Error(error);
+    }
+    return files[0]
 }
 
 function onDrop(e) {
     e.stopPropagation();
     e.preventDefault();
     // noinspection JSUnresolvedVariable
-    readFile(e.dataTransfer.files[0]);
+    readFile(safe_get_file(e.dataTransfer));
 }
 
 function initDropzone() {
