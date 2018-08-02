135ac7c463c0561628e9ae191e4cab6416bc8716
DimaDK <DimaKrutDK@ya.ru>
2018-08-02 12:29:13 +0300

    Changed config to nicely deploy dist to github, so splitted js chunks

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,29 @@
-const path = require('path');
+const path = require('path')
 
 let config = {
     entry: './src/index.js',
     output: {
-        filename: 'index.js',
+        filename: '[name].js',
         path: path.resolve(__dirname, 'dist')
     },
     module: {
         rules: [
-            {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            }
         ]
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all'
+        }
     }
-};
+}
 
-module.exports = env => {
-    if (env.mode === 'development')
-        config.devtool = 'eval-source-map';
-    config.mode = env.mode;
-    return config;
-};
+module.exports = (env) => {
+    if (env.mode === 'development') config.devtool = 'eval-source-map'
+    config.mode = env.mode
+    return config
+}
